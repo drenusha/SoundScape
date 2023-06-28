48d3e12e352cea62037c097a93cb67f88b14b018
drenusha <drenusha.tafaj@gmail.com>
2023-06-28 12:54:06 +0200

    changes in the nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,8 @@
 import { Menu, MenuProps } from "antd";
 import React, { useState } from "react";
 import "./../assetss/style/Nav.css";
+import {BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import { HomeOutlined, ProfileOutlined, LogoutOutlined } from '@ant-design/icons';
 
 const items = [
   {
@@ -15,13 +17,30 @@ const items = [
 
 const Nav = () => {
   return (
-    <div>
-      <Menu
-        className="menu"
-        mode="horizontal"
-        items={items}
-      />
-    </div>
+    <Router >
+    <Menu mode="horizontal">
+      <Menu.Item key="home" icon={<HomeOutlined />}>
+        <Link to="/">Home</Link>
+      </Menu.Item>
+      <Menu.Item key="profile" icon={<ProfileOutlined />}>
+        <Link to="/profile">Profile</Link>
+      </Menu.Item>
+      <Menu.Item key="logout" icon={<LogoutOutlined />} >
+        Logout
+      </Menu.Item>
+    </Menu>
+
+    <Route>
+      <Route exact path="/">
+        {/* Render your home component */}
+      </Route>
+      <Route path="/profile">
+        {/* Render your profile component */}
+      </Route>
+      <Navigate to="/" />
+    </Route>
+  </Router>
+
   );
 };
 export default Nav;
